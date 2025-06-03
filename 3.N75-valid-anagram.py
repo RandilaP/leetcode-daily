1f5e3a109e0db4ad3f05aa909af2651d85cd67a9
Randila Premarathne <randilamenukapremarathne@gmail.com>
2025-06-03 12:18:22 +0530

    intial and valid anagram

---
+++ 3.N75-valid-anagram.py
@@ -0,0 +1,32 @@
+"""Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ """
+def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
